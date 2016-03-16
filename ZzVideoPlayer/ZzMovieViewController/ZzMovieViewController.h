4dd9ed85af010ab47190fbada20a04306632ed06
yzz <2605387933@qq.com>
2016-03-16 11:10:35 +0800

    添加了适应横竖屏

---
+++ ZzVideoPlayer/ZzMovieViewController/ZzMovieViewController.h
@@ -0,0 +1,54 @@
+//
+//  ZzMovieViewController.h
+//  ZzVideoPlayer
+//
+//  Created by lanou on 16/3/16.
+//  Copyright © 2016年 yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+CG_INLINE CGRect CGRectMakeInline(CGFloat x, CGFloat y,CGFloat width,CGFloat height){
+    CGRect rect;
+    
+    // 1、算出缩放比例
+    //以 6S Plus 为基准
+    
+    CGFloat autoSizeX = [UIScreen mainScreen].bounds.size.width / 414;
+    CGFloat autoSizeY = [UIScreen mainScreen].bounds.size.height / 736;
+    
+    // 2 计算适配之后的X轴坐标 Y 轴坐标 以及 宽高
+    rect.origin.x = x * autoSizeX;
+    rect.origin.y = y * autoSizeY;
+    rect.size.width = width * autoSizeX;
+    rect.size.height = height * autoSizeY;
+    
+    return rect;
+}
+
+CG_INLINE CGPoint CGPointMakeInline(CGFloat x,CGFloat y){
+    CGPoint rect;
+    
+    CGFloat  autoSizeX = [UIScreen mainScreen].bounds.size.width / 414;
+    CGFloat  autoSizeY = [UIScreen mainScreen].bounds.size.height / 736;
+    
+    
+    rect.x = x * autoSizeX;
+    rect.y = y * autoSizeY;
+    return rect;
+}
+
+// 枚举值，包含水平移动方向和垂直移动方向
+typedef NS_ENUM(NSInteger, PanDirection){
+    PanDirectionHorizontalMoved,
+    PanDirectionVerticalMoved
+};
+
+@interface ZzMovieViewController : UIViewController
+
+@property (nonatomic,strong)NSString *videoURL;
+
+@end
+
+
+
